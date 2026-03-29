acbcfc8de40b3465a3421929c8624a41aa4998eb
valeria <s2m4s@unb.ca>
2026-03-29 04:24:54 -0300

    Fix character attack movement and demon form issue, added crystal collection system with GameState, and implement door fade transition for zone 1: the nave.

---
+++ scene2/crystals.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var collected: bool = false
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("player") and not collected:
+		collected = true
+		$"../Crystals".visible  = false
+		GameState.collect_crystal()
+		$"../CrystalLabel".visible = true
+		await get_tree().create_timer(1.0).timeout
+		$"../CrystalLabel".visible = false
+
+
+func _on_tp_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
