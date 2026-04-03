b21449ed1e3163cabbd67da2d176477ddc779aba
Tasfi <tasfisamad1@gmail.com>
2026-04-03 14:07:18 -0300

    blobfix

--- shadowblob.gd
+++ shadowblob.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
+
 @export var speed: float = 80.0
-@export var max_health: float = 300.0
+@export var max_health: float = 70.0
 @export var attack_damage: float = 15.0
 @export var gravity: float = 900.0
 @export var attack_cooldown: float = 1.0
 
 var health: float
 var is_dead: bool = false
+var player: Node2D = null
 var can_attack: bool = true
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var detection_area: Area2D = $DetectionArea
 @onready var attack_area: Area2D = $AttackArea
 @onready var attack_timer: Timer = $AttackTimer
 @onready var health_bar: ProgressBar = $HealthBarUI/ProgressBar
@@ -23,21 +26,40 @@ func _ready() -> void:
 	attack_timer.one_shot = true
 	attack_timer.timeout.connect(_on_attack_timer_timeout)
 	
+	detection_area.body_entered.connect(_on_detection_area_body_entered)
+	detection_area.body_exited.connect(_on_detection_area_body_exited)
 	attack_area.body_entered.connect(_on_attack_area_body_entered)
-	anim.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
+	anim.animation_finished.connect(_on_animation_finished)
+	
+	anim.play("right")
+
 func _physics_process(delta: float) -> void:
 	if is_dead:
 		return
 		
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+	if player:
+		# Face player
+		var dir_x = sign(player.global_position.x - global_position.x)
+		anim.flip_h = dir_x < 0
 		
-	velocity.x = speed
-	anim.play("right")
-	
+		# Move toward player
+		velocity.x = dir_x * speed
+		anim.play("right")
+	else:
+		velocity.x = 0
+
 	move_and_slide()
 
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
 
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = null
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
 	if is_dead or not can_attack:
@@ -49,16 +71,13 @@ func _on_attack_area_body_entered(body: Node2D) -> void:
 		can_attack = false
 		attack_timer.start()
 
-
-
 func _on_attack_timer_timeout() -> void:
 	can_attack = true
 
-
-
-func _on_animated_sprite_2d_animation_finished() -> void:
+func _on_animation_finished() -> void:
 	if anim.animation == "death":
 		queue_free()
+
 func take_damage(amount: float) -> void:
 	if is_dead:
 		return
