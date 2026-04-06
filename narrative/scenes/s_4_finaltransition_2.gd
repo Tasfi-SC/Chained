ed7ea14e38a4fb5946349b31774f605df57e722d
t42rarri <t42rarri>
2026-04-06 00:58:45 -0300

    scene 4 done

---
+++ narrative/scenes/s_4_finaltransition_2.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var label = $resizefix/BLACKSCREENUI/RichTextLabel
+const FULL_TEXT = "[center]SOMEWHERE ABOVE,\n\n A WOMAN STEPS INTO MORNING LIGHT.\n\nSHE DOES NOT LOOK BACK.\n\nSHE NEVER PLANNED TO[/center]"
+
+var is_typing = false
+
+func _ready():
+	label.scroll_following = true
+	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	label.text = FULL_TEXT
+	label.visible_characters = 0
+	await get_tree().create_timer(0.2).timeout
+	start_typing()
+
+func start_typing():
+	is_typing = true
+	var tween = create_tween()
+	tween.tween_property(label, "visible_characters", len(label.get_parsed_text()),10)
+	await tween.finished
+	is_typing = false
+
+func _input(event):
+	if event.is_action_pressed("next_line"):
+		if is_typing:
+			label.visible_characters = -1
+			is_typing = false
+		else:
+			get_tree().change_scene_to_file("res://scene4/Scene4_1.tscn")
