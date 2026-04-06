3d4476e61ebf1ce05911336157b4ff7883710f35
t42rarri <t42rarri>
2026-04-06 00:40:07 -0300

    main menu done

---
+++ main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+@onready var anim = $Node2D/AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene3/Scene3_1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
