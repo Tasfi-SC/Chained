b99d8581df8d279a0b105f6b358f6d51e03db797
Tasfi <tasfisamad1@gmail.com>
2026-04-06 03:17:25 -0300

    small fix

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene3/Scene3_1.tscn")
+	get_tree().change_scene_to_file("res://narrative/scenes/STARTGAMEDIALOGUE.tscn")
 
 
 func _on_button_2_pressed() -> void:
