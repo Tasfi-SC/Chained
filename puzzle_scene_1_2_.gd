b431009745f988c101cce7ae07c6d52edddf2c09
nazkaraman <123608268+nazkaraman@users.noreply.github.com>
2026-04-06 23:11:35 -0300

    Level 1 files completed

--- puzzle_scene_1_2_.gd
+++ puzzle_scene_1_2_.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 var light1_on = false
-var light2_on = false
+var light2_on = true
 var light3_on = false
+var light4_on = false
 
-var lever1_on = false
-var lever2_on = false
-var lever3_on = false
+# 0 = neutral, 1 = up, 2 = down
+var lever1_state = 0
+var lever2_state = 0
+var lever3_state = 0
 
 var solved = false
 var current_lever = null
@@ -14,6 +16,7 @@ var current_lever = null
 @onready var light1 = $Light1
 @onready var light2 = $Light2
 @onready var light3 = $Light3
+@onready var light4 = $Light4
 
 @onready var lever1_sprite = $Lever1/Sprite2D
 @onready var lever2_sprite = $Lever2/Sprite2D
@@ -29,10 +32,20 @@ var current_lever = null
 @export var light_off_texture: Texture2D
 @export var light_on_texture: Texture2D
 
+@export var lever_neutral_texture: Texture2D
 @export var lever_up_texture: Texture2D
 @export var lever_down_texture: Texture2D
 
 func _ready():
+	light1_on = false
+	light2_on = true
+	light3_on = false
+	light4_on = false
+
+	lever1_state = 0
+	lever2_state = 0
+	lever3_state = 0
+
 	update_lights()
 	update_levers()
 
@@ -72,61 +85,55 @@ func get_lever_label_position(lever_number):
 			return $Lever3.global_position + Vector2(-20, -50)
 	return Vector2.ZERO
 
+func advance_lever_state(current_state):
+	if current_state == 0:
+		return 2
+	elif current_state == 2:
+		return 1
+	else:
+		return 2
+
 func pull_lever(lever_number):
 	match lever_number:
 		1:
-			lever1_on = !lever1_on
+			lever1_state = advance_lever_state(lever1_state)
 			light1_on = !light1_on
 			light2_on = !light2_on
 		2:
-			lever2_on = !lever2_on
+			lever2_state = advance_lever_state(lever2_state)
 			light2_on = !light2_on
 			light3_on = !light3_on
 		3:
-			lever3_on = !lever3_on
+			lever3_state = advance_lever_state(lever3_state)
 			light1_on = !light1_on
-			light2_on = !light2_on
 			light3_on = !light3_on
+			light4_on = !light4_on
 
 	update_levers()
 	update_lights()
 
 func update_lights():
-	if light1_on:
-		light1.texture = light_on_texture
-	else:
-		light1.texture = light_off_texture
-
-	if light2_on:
-		light2.texture = light_on_texture
-	else:
-		light2.texture = light_off_texture
-
-	if light3_on:
-		light3.texture = light_on_texture
-	else:
-		light3.texture = light_off_texture
-
+	light1.texture = light_on_texture if light1_on else light_off_texture
+	light2.texture = light_on_texture if light2_on else light_off_texture
+	light3.texture = light_on_texture if light3_on else light_off_texture
+	light4.texture = light_on_texture if light4_on else light_off_texture
 	check_solution()
 
 func update_levers():
-	if lever1_on:
-		lever1_sprite.texture = lever_down_texture
-	else:
-		lever1_sprite.texture = lever_up_texture
+	update_one_lever_sprite(lever1_sprite, lever1_state)
+	update_one_lever_sprite(lever2_sprite, lever2_state)
+	update_one_lever_sprite(lever3_sprite, lever3_state)
 
-	if lever2_on:
-		lever2_sprite.texture = lever_down_texture
+func update_one_lever_sprite(sprite, state):
+	if state == 0:
+		sprite.texture = lever_neutral_texture
+	elif state == 1:
+		sprite.texture = lever_up_texture
 	else:
-		lever2_sprite.texture = lever_up_texture
-
-	if lever3_on:
-		lever3_sprite.texture = lever_down_texture
-	else:
-		lever3_sprite.texture = lever_up_texture
+		sprite.texture = lever_down_texture
 
 func check_solution():
-	if light1_on and light2_on and light3_on and not solved:
+	if light1_on and light2_on and light3_on and light4_on and not solved:
 		solved = true
 		portal.visible = true
 		interact_label.visible = false
